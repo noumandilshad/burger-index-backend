973fcbd844107cb1ef50db04026e73ae71d4b55d
noumandilshad <nouman.dilshad@gmail.com>
2024-04-14 03:49:16 +0500

    feat():: implemented elastic-search feature

---
+++ src/modules/data-management/elastic-search.service.ts
@@ -0,0 +1,50 @@
+// src/services/elasticsearch.service.ts
+import { HttpException, Injectable } from '@nestjs/common';
+import { Client } from '@elastic/elasticsearch';
+
+@Injectable()
+export class ElasticSearchService {
+  private readonly client: Client;
+
+  constructor() {
+    this.client = new Client({
+      node: process.env.ELASTIC_SEARCH_URL,
+      auth: {
+        username: process.env.ELASTIC_SEARCH_USERNAME,
+        password: process.env.ELASTIC_SEARCH_PASSWORD,
+      },
+    });
+  }
+
+  async indexDocument(index: string, body: any): Promise<void> {
+    try {
+      await this.client.index({
+        index,
+        body,
+      });
+      console.log(`Document indexed successfully in index ${index}`);
+    } catch (error) {
+      console.error(`Error indexing document in index ${index}:`, error);
+    }
+  }
+
+  async search(index: string, query: any): Promise<any[]> {
+    console.log({ index, query });
+    const response: any = await this.client.search({
+      index,
+      body: {
+        query: {
+          bool: {
+            should: query,
+          },
+        },
+      },
+    });
+
+    if (!response || !response?.hits || !response?.hits?.hits.length) {
+      throw new HttpException('No data found.', 404);
+    }
+
+    return response.hits?.hits.map((hit) => hit._source);
+  }
+}
